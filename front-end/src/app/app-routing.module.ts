0a9e23ecfa2cf52798f39a2bb50fd186308c1323
sara-gaballa <96298154+sara-gaballa@users.noreply.github.com>
2022-12-25 02:49:21 +0200

    start page test

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailComponent } from './email/email.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'mail',
+    component:EmailComponent
+  },
+  {
+    path:'',
+    component:StartPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmailComponent } from './email/email.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmailComponent
+    EmailComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
