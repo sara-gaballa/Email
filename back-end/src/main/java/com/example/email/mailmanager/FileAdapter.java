223030c3c12769b6035dec0b9c12423de935a0ed
Mariam Hossam <m.hossam2551@gmail.com>
2022-12-26 17:36:37 +0200

    adapter

---
+++ back-end/src/main/java/com/example/email/mailmanager/FileAdapter.java
@@ -0,0 +1,61 @@
+package com.example.email.mailmanager;
+
+import com.example.email.model.Email;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileAdapter implements MailManager {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    // return all mails in specific folder
+    @Override
+    public List<Email> getAllMails(String path) throws IOException {
+        File[] files = FileManager.getAllFiles(path);
+        List<Email> emails = new ArrayList<>();
+        for (File file : files) {
+            emails.add(objectMapper.readValue(file, Email.class));
+        }
+        return emails;
+    }
+
+    // return one mail with given folder and name
+    @Override
+    public Email getMail(String path, String name) throws IOException {
+        File file = FileManager.getFile(path, name);
+        return objectMapper.readValue(file, Email.class);
+    }
+
+    // delete all mails with the given named from the given folder
+    @Override
+    public void deleteMails(String path, String[] fileNames) {
+        for (String name : fileNames) {
+            name = name.concat(".json");
+        }
+        FileManager.deleteFiles(path, fileNames);
+    }
+
+    // add mail to the given path
+    @Override
+    public void addMail(String path, String fileName, Email email) throws IOException {
+        File file = FileManager.addFile(path, fileName + ".json");
+        //configure objectMapper for pretty input
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        //write email object to json file
+        objectMapper.writeValue(file, email);
+    }
+
+    // move mails from one folder to another
+    @Override
+    public void moveMails(String fromPath, String toPath, String[] fileNames) {
+        for (String name : fileNames) {
+            name = name.concat(".json");
+        }
+        FileManager.moveFiles(fromPath, toPath, fileNames);
+    }
+}
---
+++ back-end/src/main/java/com/example/email/mailmanager/MailManager.java
@@ -0,0 +1,27 @@
+package com.example.email.mailmanager;
+
+import com.example.email.model.Email;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface MailManager {//TODO: write email
+    // TODO : get folders on sign in
+
+    // get all emails from inbox folder
+    //public List<Email> getAllMails(User user) throws IOException;
+
+    // get all emails in specific folder
+    List<Email> getAllMails(String path) throws IOException;
+
+    // Check: Queue -> Array
+    Email getMail(String path, String name) throws IOException;
+
+
+    void addMail(String path, String fileName, Email email) throws IOException;
+
+    // TODO : add ID
+    void deleteMails(String path, String[] ids);
+
+    void moveMails(String fromPath, String toPath, String[] ids);
+}
