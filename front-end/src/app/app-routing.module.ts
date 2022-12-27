d185b337bbea57519b9d6b017fb83fbf8a306672
NancyHisham <96303049+NancyHisham@users.noreply.github.com>
2022-12-27 03:30:26 +0200

    compose - mail-box components

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComposeEmailComponent } from './components/compose-email/compose-email.component';
 import { InboxComponent } from './components/inbox/inbox.component';
+import { MailBoxComponent } from './components/mail-box/mail-box.component';
 import { StartPageComponent } from './components/start-page/start-page.component';
+import { MailBox } from './model/Email';
 
 const routes: Routes = [
   {
@@ -11,6 +14,14 @@ const routes: Routes = [
   {
     path:'',
     component:StartPageComponent,
+  },
+  {
+    path:'mail-box',
+    component:MailBoxComponent,
+  },
+  {
+    path:'compose',
+    component:ComposeEmailComponent,
   },
 ];
 
--- front-end/src/app/components/mail-box/mail-box.component.ts
+++ front-end/src/app/components/mail-box/mail-box.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './mail-box.component.html',
   styleUrls: ['./mail-box.component.css']
 })
+
 export class MailBoxComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  constructor() {
+    
+   }
+  getSubject():string{
+    return "Subject";
+  }
+  getSenderName():string{
+    return "Nancy";
+  }
+  getSenderEmail():string{
+    return "NancyHisham@galaxy.com";
+  }
+  getDate():string{
+    return "12/27/2022"
+  }
+  getTo():string{
+    return "SaraGaballah@galaxy.com"
+  }
+  getbody():string{
+    return "body"
   }
 
 }
